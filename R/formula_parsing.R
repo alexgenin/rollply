b00e9d3fecd93aeb9c2c906e8a16032543fc51fa
Alexandre Génin <alexandre.genin@ens-lyon.fr>
2014-10-15 15:26:17 +0200

    Reorganisation of files to prepare for the creation of a package.

---
+++ R/formula_parsing.R
@@ -0,0 +1,34 @@
+# 
+# 
+# Given a formula with a "|" in it, splits it into two formulas
+# 
+
+parse_roll_formula <- function(form) {
+  
+  
+  if (has_groups(form)) { 
+    output <- split_groups(form)
+  } else {
+    output <- list(vars=as.formula(paste0('~',form[2])),
+                   groups=NULL)
+  }
+  return(output)
+}
+
+split_groups <- function(form) {
+  
+  # Get string representation
+  form_str <- as.character(form[[2]])
+  
+  # Retrieve symbols
+  vars   <- as.formula(paste0('~',form_str[2]))
+  groups <- as.formula(paste0('~',form_str[3]))
+  return(list(groups=groups, vars=vars))
+}
+
+# Function that tests if there are groups passed (finds a "|" in the formula
+# definition
+has_groups <- function(form) {
+  form_str <- paste(as.character(form),collapse='')
+  return( grepl('\\|',form_str) ) 
+}
