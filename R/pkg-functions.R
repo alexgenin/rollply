01302275f28186b83dd1dc9552a5113bf6c22b63
Alexandre Génin <alexandre.genin@ens-lyon.fr>
2014-10-27 16:43:06 +0100

    Added loading of plyr to onAttach

---
+++ R/pkg-functions.R
@@ -0,0 +1,18 @@
+#
+# 
+# 
+# 
+# This file contains functions that deal with package lodaing and unloading.
+# 
+
+
+.onAttach <- function(libname, pkgname) {
+  
+  # We load plyr with rollply as many functions will be directly useful to 
+  # interactive work with rollply (e.g. summarise)
+  if (!"package:plyr" %in% search()) {
+    pkgStartupMessage('Attaching package plyr')
+    library(plyr, quietly=TRUE)
+  }
+  
+}
