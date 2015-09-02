de3f133aa92cc2119a831283de992604bb5b5eee
Alexandre Génin <alex@lecairn.org>
2015-09-02 15:05:36 +0200

    Fixed stuff to pass CRAN checks

--- R/build_grid_ahull_fill.R
+++ R/build_grid_ahull_fill.R
@@ -2,94 +2,98 @@
 #' @title Regular grid in alpha-hull
 #'
 #' @description Create a grid within the alpha hull of a set of points.
-#' 
-#' @details This function computes the alpha hull of a set of points, then 
+#'
+#' @details This function computes the alpha hull of a set of points, then
 #'          iteratively finds the best grid of \code{npts} points fitting in the hull.
 #'
-#' 
+#'
 #' @param coords A matrix or data.frame of coordinates with two columns
 #' @param npts The approximate number of points of the requested grid
 #' @param pad ignored.
-#' @param grid_opts A list with component \code{alpha} that controls the 
+#' @param grid_opts A list with component \code{alpha} that controls the
 #'                     shape of the alpha hull, \code{error_tol} the error
-#'                     tolerance as a fraction of the number of points, 
-#'                     \code{run_max} the maximum number of iterations and 
+#'                     tolerance as a fraction of the number of points,
+#'                     \code{run_max} the maximum number of iterations and
 #'                     \code{verbose} to get some debug output.
-#' 
-#' @return The coordinates of a grid of points as a \code{data.frame} with 
+#'
+#' @return The coordinates of a grid of points as a \code{data.frame} with
 #'         approximately \code{npts} rows and \code{ncol(coords)} columns. Names
 #'         are transfered from the \code{coords} data frame.
-#' 
+#'
 #' @family grid building functions
-#' 
+#'
 #' @export
-build_grid_ahull_fill <- function(coords, npts, 
+build_grid_ahull_fill <- function(coords, npts,
                                   pad = NULL, # pad is ignored for this function
-                                  grid_opts = list(alpha = .3, 
-                                                   error_tol = .05, 
-                                                   run_max = 20, 
+                                  grid_opts = list(alpha = .3,
+                                                   error_tol = .05,
+                                                   run_max = 20,
                                                    verbose = FALSE)) {
-  
+
+  if ( !require(alphahull) ) {
+    stop('Alpha-hull-based grids require package alphahull')
+  }
+
   build_grid_check_vars(coords, npts)
-  
+
   # Remove duplicates otherwise ahull throws an error
   coords <- coords[! duplicated(coords), ]
-  
+
   if (ncol(coords)!=2) {
     stop('This type of grid is only implemented for 2 dimensions.')
   }
-  
+
   # Take parameters into account
   opts <- list(alpha = .3, error_tol = .05, run_max = 20, verbose = FALSE)
   opts[names(grid_opts)] <- grid_opts # alter defaults
   verbose <- opts[['verbose']]
-  
+
   # We build an alpha hull of our x/y points.
-  # NB: We rescale everything between 0 and 1 so the given alpha is easy to 
+  # NB: We rescale everything between 0 and 1 so the given alpha is easy to
   #     interpret and inahull() works (it has trouble with far-from zero values
   #     it seems).
-  # <!todo!> this introduces a bug in grid_square tile and gives it the 
+  # <!todo!> this introduces a bug in grid_square tile and gives it the
   # same behavior than grid_identical
   coords.scaled <- apply(coords, 2, scales::rescale, to = c(0,1))
   coords.hull   <- alphahull::ahull(coords.scaled, alpha = opts[['alpha']])
-  
+
   if (verbose) cat('Building grid in alphahull...\n')
-  
+
   # Iterate to find the closest number of points
   error <- -1
   run <- 1
   npts_target <- npts
   while ( error < - opts[['error_tol']] && run <= opts[['run_max']] ) {
-    
+
     # Compute a grid with the given number of points
     rect_grid <- build_grid_squaretile(coords.scaled, npts)
-    
+
     # Strip points and see how many fall in alphahull
-    to_keep <- apply(rect_grid, 1, 
+    to_keep <- apply(rect_grid, 1,
                      function(X) alphahull::inahull(coords.hull, X))
     error <- (sum(to_keep) - npts_target) / npts_target
-    
-    if (verbose) { 
-      cat(paste0('run ', run,', error=', round(error, digits = 2), 
+
+    if (verbose) {
+      cat(paste0('run ', run,', error=', round(error, digits = 2),
                 '% (', sum(to_keep), ' points)\n'));
     }
-    
+
     # Increase number of points
     run <- run + 1
     npts <- round(npts - error * npts)
   }
-  
+
   # Scale back to original size
   grid <- rect_grid[to_keep, ]
   grid <- data.frame(x = grid[ ,1]*diff(range(coords[ ,1])) + min(coords[ ,1]),
                      y = grid[ ,2]*diff(range(coords[ ,2])) + min(coords[ ,2]))
-  
+
   return(grid)
 }
 
 # Debug snippet: makes an output with a plot
 #   plot(hull)
 #   points(grid, col='red', pch=3)
-#   title(paste0('run ', run, ' (error: ', 
+#   title(paste0('run ', run, ' (error: ',
 #                paste0(round( error * 100 )), '%)', sep=''))
 
--- R/rollply.R
+++ R/rollply.R
@@ -1,99 +1,99 @@
-# 
+#
 # rollply: split-apply-combine strategy for moving-window computations.
-# Originally written by Alexandre Génin, 2014
-# 
-# Note: sometimes parallel computing warns that "..." is used in an 
-#   incorrect context. This is probably related to [1] and is unfixed at the 
+# Originally written by Alexandre Genin, 2014
+#
+# Note: sometimes parallel computing warns that "..." is used in an
+#   incorrect context. This is probably related to [1] and is unfixed at the
 #   moment.
-# 
+#
 # [1] https://github.com/hadley/plyr/issues/203
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 #' @title Rollply
-#' 
-#' @description Applies a function on data falling in a moving window, then 
+#'
+#' @description Applies a function on data falling in a moving window, then
 #{              combine results in a data.frame.
-#' 
+#'
 #' @param .data \code{data.frame} to be processed
-#' @param .rollvars variables describing the moving window: a formula of the 
-#'                  form \code{~ a + b | c}, where \code{a} and \code{b} denote 
-#'                  the variables used for the rolling window and \code{c} an 
+#' @param .rollvars variables describing the moving window: a formula of the
+#'                  form \code{~ a + b | c}, where \code{a} and \code{b} denote
+#'                  the variables used for the rolling window and \code{c} an
 #'                  optional grouping variable
 #' @param fun function to apply
 #' @param wdw.size window size
-#' @param grid data.frame of points at which the computation is done. If 
+#' @param grid data.frame of points at which the computation is done. If
 #'             \code{NULL} then a grid is generated using \code{grid_npts}
 #'             and \code{grid_type}.
-#' @param grid_npts if grid is unspecified, then the number of points for the 
+#' @param grid_npts if grid is unspecified, then the number of points for the
 #'                  resolution of the grid to build. Otherwise ignored.
 #' @param grid_type The type of grid to generate
 #' @param grid_opts Options to be passed to the grid-generating function
-#' @param padding padding policy one the edgse of the dataset, one of 'none', 
-#'                'outside', 'inside', or a numeric value 
-#' @param .parallel whether to use parallel processing (see \code{\link{ddply}} 
-#'                  for more information on parallelism). 
+#' @param padding padding policy one the edgse of the dataset, one of 'none',
+#'                'outside', 'inside', or a numeric value
+#' @param .parallel whether to use parallel processing (see \code{\link{ddply}}
+#'                  for more information on parallelism).
 #' @param ... other arguments passed to \code{\link{ddply}} and \code{fun}
-#' 
+#'
 #' @details
-#' 
-#' Rollply applies a function one or more variables. It is built internally over ddply and follows a very 
-#' similar syntax. 
-#' 
-#' @return 
+#'
+#' Rollply applies a function one or more variables. It is built internally over ddply and follows a very
+#' similar syntax.
+#'
+#' @return
 #' A named data.frame with the function results at each grid point.
-#' 
+#'
 #' @examples
-#' 
+#'
 #' library(plyr)
 #' # 1D example
 #' dat <- data.frame(time=seq.int(1000),
 #'                   position=cumsum(rnorm(1000,0,10)))
-#' 
-#' rollav <- rollply(dat, ~ time, wdw.size=10, 
+#'
+#' rollav <- rollply(dat, ~ time, wdw.size=10,
 #'                   summarise, position=mean(position))
-#' 
+#'
 #' plot(position ~ time, data=dat)
 #' lines(rollav, col='red')
-#' 
+#'
 #' # 2D example
-#' 
+#'
 #' # Generate three 2D random walks
-#' dat <- ddply(data.frame(person=c('francois','nicolas','jacques')), ~ person, 
-#'              summarise, 
+#' dat <- ddply(data.frame(person=c('francois','nicolas','jacques')), ~ person,
+#'              summarise,
 #'               time=seq.int(1000),
 #'               x=cumsum(rnorm(1000,0,1)),
 #'               y=cumsum(rnorm(1000,0,1)))
-#' 
+#'
 #' # Smoothed trajectory over ten time-steps
 #' rollav <- rollply(dat, ~ time | person, wdw.size=10, grid_res=1000,
 #'                   summarise, x=mean(x), y=mean(y))
-#' 
+#'
 #' library(ggplot2)
-#' if (require(ggplot2)) { 
-#' ggplot(dat,aes(x,y,color=person)) + 
-#'   geom_point(alpha=.5, shape='+') + 
-#'   geom_path(data=rollav) 
+#' if (require(ggplot2)) {
+#' ggplot(dat,aes(x,y,color=person)) +
+#'   geom_point(alpha=.5, shape='+') +
+#'   geom_path(data=rollav)
 #' }
-#'  
+#'
 #  # Where did people spend their time ?
 #'  fixed_grid <- build_grid_squaretile(dat[ ,c('x','y')], 2000) # we fix the grid across groups
 #'  rollav <- rollply(dat, ~ x + y | person, wdw.size=2, grid=fixed_grid,
 #'                    summarise, time.spent=length(time))
-#'  
-#' if (require(ggplot2)) { 
-#'   ggplot(subset(rollav, time.spent>0)) + 
-#'     geom_point(aes(x,y, color=person, size=time.spent), alpha = .5) + 
+#'
+#' if (require(ggplot2)) {
+#'   ggplot(subset(rollav, time.spent>0)) +
+#'     geom_point(aes(x,y, color=person, size=time.spent), alpha = .5) +
 #'     facet_grid(~person)
 #' }
-#' 
+#'
 #' # see also vignette(rollply_intro)
-#' 
+#'
 #' @useDynLib rollply
 #' @importFrom Rcpp sourceCpp
 #' @export
-#' 
+#'
 
 rollply <- function(.data,
                     .rollvars,
@@ -106,11 +106,11 @@ rollply <- function(.data,
                     padding   = 'none', # outside/inside/none or value
                     .parallel = FALSE,
                     ...) {  # passed to ddply/fun
-  
+
   vars <- parse_formula(.rollvars, enclos=parent.frame())
-    
+
   check_args(vars, .data, grid, grid_type, wdw.size)
-  
+
   # Handle groups: if we provide groups, then we call rollply within each
   # groups using ddply.
   if ( ! is.na(vars[["groups"]]) ) {
@@ -124,22 +124,22 @@ rollply <- function(.data,
     # Call ddply
     return( do.call(plyr::ddply, args.grps, envir=parent.frame()) )
   }
-  
+
   # We extract variables used for computing and build a matrix
   # <!todo!> Add check that variables used for rolling windows are numeric!
-  coords <- plyr::eval.quoted(vars[["coords"]], 
+  coords <- plyr::eval.quoted(vars[["coords"]],
                               envir=as.data.frame(.data))
   coords <- do.call(cbind, coords)
-  
-  # Set the number of points for the grid 
-  if ( is.null(grid_npts) ) { 
-    coords_span <- apply(coords, 2, function(x) diff(range(x))) 
+
+  # Set the number of points for the grid
+  if ( is.null(grid_npts) ) {
+    coords_span <- apply(coords, 2, function(x) diff(range(x)))
     grid_npts <- floor(max(coords_span / wdw.size ^ ncol(coords)) )
   }
-  
+
   # Check for NAs, zero area, etc
   check_coords(coords, grid)
-  
+
   # Determine sides policy
   if (!is.numeric(padding)) {
     padding <- switch(padding,
@@ -147,21 +147,21 @@ rollply <- function(.data,
                       inside  = - wdw.size / 2,
                       none    = 0)
   }
-  
+
   # Build output grid
   if (is.null(grid)) {
     grid <- build_grid(grid_type, coords, grid_npts, padding, grid_opts)
-  } else if ( ! is.data.frame(grid)) { 
+  } else if ( ! is.data.frame(grid)) {
     grid <- as.data.frame(grid)
   }
-    
-  # Do the work 
-  # Note: we use ddply here as it has a more robust behaviour than adply, 
+
+  # Do the work
+  # Note: we use ddply here as it has a more robust behaviour than adply,
   # (most notably when the inner function returns a data.frame with multiple
   # lines).
   result <- plyr::ddply(grid, names(grid),
                         do_rollply, coords, wdw.size, .data, fun,
                         ...)
-  
+
   return(result)
 }
